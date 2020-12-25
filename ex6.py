40be56be98debb2433b3a22c9a07f529ceb3c6b9
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2020-12-25 14:16:15 +0200

    Update ex6.py
    Functions 6 better designed

--- ex6.py
+++ ex6.py
@@ -38,42 +38,21 @@ def nested_list_sum(lst):
 
 
 # 6
-def the_winner_is(n, total_of_cals=0):
-    """this function calculates who would win in a Nim game,
-    when both players play an optimal game. in addition, it
-    calculate the number of recursion calls"""
-    # total_of_cals default is 0
-    total_of_cals = total_of_cals
-    # if we got to the situation where n == 0, it means that the first
-    # player loosed.
-    if n == 0:
-        print("Player 1 lose with {} calls".format(total_of_cals))
-        return False
-    # since the rule of the game is that in any turn a player can take
-    # one, or two or four coins, in these situations the player will win,
-    # and live an empty board
-    elif n == 1 or n == 2 or n == 4:
-        print("Player 1 won with {} calls".format(total_of_cals))
-        return True
-    else:
-        # we will simplified the game using recursion, now with -3 coins
-        total_of_cals += 1
-        return the_winner_is(abs(n - 3), total_of_cals)
-
-
-def coin_pick_winner(n):
-    """this function return True if the first layer will win, and Falls is he loose
-    in addition it calculate the number of option that he has in order to win at
-    the first step/ or, if he loose, the opposite"""
-    first_step = the_winner_is(n)
-    if n >= 9:
-        if n % 3 == 0:
-            return first_step, 3
-        elif n % 3 == 1:
-            return first_step, 2
-        else:
-            return first_step, 1
-    elif n < 9 and n != 3 and n != 4:
-        return first_step, 1
-    else:
-        return first_step, 2
+def coin_pick_winner(n, total_of_cals=0):
+    """this function calculates who would win in a Nim game, when both players
+    play an optimal game. in addition, it calculate the number of recursion calls,
+    and the number of possible optionsin the first step for winning, or for loosing
+    if player B is the winner"""
+    if n == 1 or n == 2:
+        print("player 1 won: {} recursion calls were made".format(total_of_cals))
+        # True means here that player 1 has one. the number represents the number of
+        # possible steps at his first turn, for achieve his winning.
+        return True, 1
+    elif n == 3:
+        return False, 2
+    elif n == 4:
+        return True, 2
+    elif n == 6:
+        return False, 3
+    total_of_cals += 1
+    return coin_pick_winner(n-3, total_of_cals)
